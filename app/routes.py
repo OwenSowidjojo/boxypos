41e20b3e3a4f96e58b0eccf1c9b112923acce171
jodysowidjojo <Sowidjj@hva.nl>
2025-07-27 23:18:53 +0200

    Add: category to product route

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,5 @@
+from unicodedata import category
+
 from flask import Blueprint, render_template, request, redirect, url_for
 from .models import Product
 from . import db
@@ -13,12 +15,13 @@ def menu():
 @bp.route('/add', methods=['POST'])
 def add_product():
     name = request.form.get('name')
+    category = request.form.get('category')
     price = request.form.get('price')
 
     if name and price:
         try:
             price = float(price)
-            product = Product(name=name, price=price)
+            product = Product(name=name, category= category, price =price)
             db.session.add(product)
             db.session.commit()
 
