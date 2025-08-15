3e0e862d9481821471bf7e6c24751e48d6442e6e
jodysowidjojo <Sowidjj@hva.nl>
2025-08-15 23:18:15 +0200

    Add: new order function

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,7 @@
-from datetime import date
-
+from datetime import date, datetime
 from flask import Blueprint, render_template, request, redirect, url_for
 from sqlalchemy import func
-
-from .models import Product, Order
+from .models import Product, Order, OrderItem
 from . import db
 from print.printer import print_order
 
@@ -54,9 +52,39 @@ def add_product():
     return redirect(url_for('main.menu'))
 
 
-@bp.route('/new-order')
+@bp.route('/new-order', methods=['GET', 'POST'])
 def new_order():
-    return render_template('new-order.html')
+    products = Product.query.all()
+
+    if request.method == 'POST':
+        table_number = request.form.get('table_number')
+        selected_products = request.form.getlist('product_id')
+        quantities = request.form.getlist('quantity')
+
+        order = Order(timestamp=datetime.utcnow(), total=0.0, table_number=table_number)
+        db.session.add(order)
+        db.session.flush()  # To get order.id
+
+        total_price = 0.0
+        for product_id, qty in zip(selected_products, quantities):
+            if qty and int(qty) > 0:
+                product = Product.query.get(int(product_id))
+                order_item = OrderItem(
+                    order_id=order.id,
+                    product_id=product.id,
+                    quantity=int(qty)
+                )
+                db.session.add(order_item)
+                total_price += product.price * int(qty)
+
+        order.total = total_price
+        db.session.commit()
+
+
+        return redirect(url_for('main.home'))
+
+    return render_template('new-order.html', products=products)
+
 
 @bp.route("/history")
 def history():
@@ -66,6 +94,3 @@ def history():
 def admin():
     return render_template('admin.html')
 
-# @bp.route("/new-order")
-# def new_order():
-#     return render_template('new-order.html')
